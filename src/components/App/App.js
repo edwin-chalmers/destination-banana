b10f856c235a831cc5e3ef0c52754ad25f439851
Adam Bedient <adambedient@gmail.com>
2024-04-04 13:50:11 -0400

    Display link titles on page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,53 @@
 
 import './App.css';
 import WikiPage from '../WikiPage/WikiPage'
+import LinkBox from '../LinkBox/LinkBox'
 import { fetchPage } from '../../ApiCalls';
 import { useEffect, useState } from 'react'
 import parse from 'html-react-parser';
 
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('')
+  const [currentPage, setCurrentPage] = useState({})
+  const [linkList, setLinkList] = useState([])
 
   useEffect(() => {
+    const parser = new DOMParser()
     fetchPage('banana')
       .then(response => {
         return response.text()
       })
       .then(html => {
-        const parsedHTML = parse(html)
-        setCurrentPage(parsedHTML)
+        const parsedHTMLforLinks = parser.parseFromString(html, 'text/html')
+        const parsedHTMLforDOM = parse(html)  
+        // console.log("parsedHTML", parsedHTML)
+        setCurrentPage({stringForLinks: parsedHTMLforLinks, stringForDOM: parsedHTMLforDOM})
+        createLinkList(parsedHTMLforLinks)
       })
   }, [])
 
-  function createLinkList () {
-    
+  // useEffect(() => {
+  //   if(currentPage.stringForLinks) {
+  //     createLinkList(currentPage.stringForLinks)
+  //   }
+  // }, [currentPage])
+
+  function createLinkList (htmlString) {
+    const wikiLinks = []
+    const linkNodes = htmlString.querySelectorAll('a')
+    const wikiNodes = linkNodes.forEach((linkNode) => {
+      if (linkNode.href.includes('wikipedia')) {
+        wikiLinks.push(linkNode.href)
+      }
+    })
+    setLinkList(wikiLinks)
   }
 
   return (
-    <>
-      <WikiPage pageHTML={currentPage} />
-      <LinkBox links={linkList}/>
-    </>
+    <main>
+      <WikiPage pageHTML={currentPage.stringForDOM} />
+      <LinkBox linkList={linkList}/>
+    </main>
   );
 }
 
