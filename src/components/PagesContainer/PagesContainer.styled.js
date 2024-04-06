e1326a0bd535612b50d1297fb3b66c23d4bfc72e
Edwin Chalmers <edwinchalmers@gmail.com>
2024-04-05 23:59:55 -0600

    refactored some CSS into styled components

---
+++ src/components/PagesContainer/PagesContainer.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledPagesContainer = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: flex-end;
+    align-items: flex-start;
+    flex-wrap: nowrap;
+`
