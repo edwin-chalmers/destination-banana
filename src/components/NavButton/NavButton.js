5e39d2094688e8791e8e721646a8edd8d1cf8b58
Edwin Chalmers <edwinchalmers@gmail.com>
2024-04-06 20:30:28 -0600

    Complete styling for NavButton

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -3,9 +3,15 @@ import { StyledNavButton } from './NavButton.styled'
 export default function NavButton() {
 
     return (
+        <>
         <StyledNavButton>
-             <img src='/assets/history_white_48dp.svg' alt='undo icon'/>
+            <p>back</p>
+            <div>
+                <img src='/assets/history_white_48dp.svg' alt='undo icon'/>
+            </div>
         </StyledNavButton>
+        </>
+        
     )
 
 }
--- src/components/NavButton/NavButton.styled.js
+++ src/components/NavButton/NavButton.styled.js
@@ -1,15 +1,57 @@
 import styled from 'styled-components'
 
-export const StyledNavButton = styled.div`
-    width: 40px;
-    height: 40px;
-    background-color: gray;
-    border-radius: 3px;
+const centerElements = () => `
     display: flex;
     justify-content: center;
     align-items: center;
+`
 
+export const StyledNavButton = styled.div`
+    --button-color: gray;
+    --hover-color: rgb(53, 98, 201);
+
+    ${centerElements};
+    flex-direction: column;
+
+    &:hover {
+
+        p {
+            color: var(--hover-color);
+            cursor: pointer;
+        }
+
+        div {
+            cursor: pointer;
+            background-color: var(--hover-color);
+        }
+    }
+    
+    div {
+        width: 40px;
+        height: 40px;
+        background-color: var(--button-color);
+        border-radius: 3px;
+        ${centerElements}
+    }
+    
     img {
         width: 30px;
     }
+    
+    p {
+        margin: 0;
+        color: var(--button-color); 
+        font-weight: bold;
+        font-size: small;
+    }
 `
+
+const boxMixin = (size = '100px', backgroundColor = 'lightblue') => `
+  width: ${size};
+  height: ${size};
+  background-color: ${backgroundColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+`;
