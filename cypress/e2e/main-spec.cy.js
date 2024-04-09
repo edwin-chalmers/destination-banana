5b173b7e19a9ab26ee65ee2a80ee84f37141a256
Adam Bedient <adambedient@gmail.com>
2024-04-09 17:03:13 -0400

    Establish banana intercept

--- cypress/e2e/main-spec.cy.js
+++ cypress/e2e/main-spec.cy.js
@@ -10,8 +10,8 @@ describe('destination: bananas', () => {
       cy.intercept('GET', 'https://en.wikipedia.org/api/rest_v1/page/random/title', {
       status: 200,
       body: randomPage
+      })
     })
-  })
 
     cy.intercept('GET', 'https://en.wikipedia.org/w/api.php?action=parse&page=The+Monkees&prop=links&format=json&origin=*', {
       status: 200,
@@ -21,16 +21,16 @@ describe('destination: bananas', () => {
             {'*': 'gorilla', 'exists': '', ns: 0},
             {'*': 'jungle', 'exists': '', ns: 0},
             {'*': 'music', 'exists': '', ns: 0},
-            {'*': 'The Monkees', 'exists': '', ns: 0}
+            {'*': 'The Monkees', 'exists': '', ns: 0},
+            {'*': 'banana', 'exists': '', ns: 0}
           ],
           pageid: 31417,
           title: "The Monkees"
         }
       }
-
     })
 
-    cy.fixture('htmlString.txt').then(htmlString => {
+    cy.fixture('monkeesHtmlString.txt').then(htmlString => {
       cy.intercept('GET', 'https://en.wikipedia.org/api/rest_v1/page/html/The%20Monkees', {
         status: 200,
         body: htmlString
@@ -52,22 +52,56 @@ describe('destination: bananas', () => {
       .get('#links-container').contains('a', 'jungle')
       .get('#links-container').contains('a', 'music')
       .get('#links-container').contains('a', 'The Monkees')
+      .get('#links-container').contains('a', 'banana')
 
       cy.get('#page-container').find('img').should('have.attr', 'src').should('include', '/assets/link-icon.svg')
       .get('#page-container').contains('h3', 'The Monkees')
       // .get('#page-container').contains('p', 'This article')
       // .get('#page-container').contains('a', 'music')
       // .get('#page-container').contains('a', 'The Monkees')
+
+      // cy.get('#links-container')
+      // cy.contains('a', 'banana')
+
   })
 
-  // it('should play the game, and use the back button to undo changes', () => {
+  it('should play the game, and use the back button to undo changes', () => {
+    cy.fixture('randomPage.json').then(randomPage => {
+      cy.intercept('GET', 'https://en.wikipedia.org/api/rest_v1/page/random/title', {
+      status: 200,
+      body: randomPage
+      })
+    })
 
+    cy.intercept('GET', 'https://en.wikipedia.org/w/api.php?action=parse&page=The+Monkees&prop=links&format=json&origin=*', {
+      status: 200,
+      body: {
+        parse: {
+          links: [
+            {'*': 'gorilla', 'exists': '', ns: 0},
+            {'*': 'jungle', 'exists': '', ns: 0},
+            {'*': 'music', 'exists': '', ns: 0},
+            {'*': 'The Monkees', 'exists': '', ns: 0},
+            {'*': 'banana', 'exists': '', ns: 0}
+          ],
+          pageid: 31417,
+          title: "The Monkees"
+        }
+      }
+    })
 
+    cy.fixture('monkeesHtmlString.txt').then(htmlString => {
+      cy.intercept('GET', 'https://en.wikipedia.org/api/rest_v1/page/html/The%20Monkees', {
+        status: 200,
+        body: htmlString
+      })
+    })
 
-
- 
-    // cy.visit('http://localhost:3000/HomePage')
-    // cy.get('#links-container')
+    cy.visit('http://localhost:3000/HomePage')
+    cy.get('#links-container')
+      .contains('a', 'banana')
+      .click()
+    
     // cy.contains('a', 'fish')
     // cy.contains('a', 'Immune system').click()
     // cy.contains('a', 'Animal').click()
@@ -77,6 +111,6 @@ describe('destination: bananas', () => {
     // cy.contains('a', 'Algae').click()
     // cy.contains('a', 'Plant').click()
     // cy.get('p', 'back')
-  // })
+  })
 
 })
